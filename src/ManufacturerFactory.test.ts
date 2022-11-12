7e0ffafc4c5f749fe30fafd027f5ded149f529ba
Baptiste Delhomme <baptdelhomme@gmail.com>
2022-11-12 16:14:05 +0100

    FIRST UNIT TEST WITH JEST

--- src/ManufacturerFactory.test.ts
+++ src/ManufacturerFactory.test.ts
@@ -1,16 +1,53 @@
 import {describe, expect, test} from '@jest/globals';
-import { ISensorManufacturer,ManufacturerFactory } from "./ManufacturerFactory"
+import { ISensorManufacturer,ManufacturerFactory, ManufacturerNASA, ManufacturerTESLA } from "./ManufacturerFactory"
+import Missile from './Missile';
+import { Sensor, SensorDefaut, sensorType } from './Sensor';
 import Shield from './Shield';
 
-describe('ManufacturerNASA module', () => {
-    const NASAmanufact: ISensorManufacturer = ManufacturerFactory.createManufacturer("NASA")
-    test('ManufacturerNASA name is NASA', () => {
+const NASAmanufact: ISensorManufacturer = ManufacturerFactory.createManufacturer("NASA")
+const TESLAmanufact: ISensorManufacturer = ManufacturerFactory.createManufacturer("TESLA")
+
+describe('ManufacturerFactory class', () => {
+    test('ManufacturerFactory createManufacturer("NASA") return type ManufacturerNASA', () => {
+        expect(NASAmanufact).toBeInstanceOf(ManufacturerNASA);
+    });
+    test('ManufacturerFactory createManufacturer("TESLA") return type ManufacturerTESLA', () => {
+        expect(TESLAmanufact).toBeInstanceOf(ManufacturerTESLA);
+    });
+});
+
+describe('ManufacturerNASA class', () => {
+    test('ManufacturerNASA getManufacturerName is NASA', () => {
         expect(NASAmanufact.getManufacturerName()).toBe("NASA");
+    });
+    test('ManufacturerNASA createSensor return is type of Sensor and props are accessibles', () => {
+        const sensor1 = NASAmanufact.createSensor(3,sensorType.HEAT,40);
+        expect(sensor1).toBeInstanceOf(SensorDefaut);
+        expect(sensor1.sensorType).toBe(sensorType.HEAT);
+        expect(sensor1.sensorPower).toBe(40);
     });
     test('ManufacturerNASA createShield return is type of Shield', () => {
         expect(NASAmanufact.createShield()).toBeInstanceOf(Shield);
     });
-    // test('ManufacturerNASA name is NASA', () => {
-    //     expect(NASAmanufact.getManufacturerName()).toBe("NASA");
-    // });
+    test('ManufacturerNASA createMissile return is type of Missile', () => {
+        expect(NASAmanufact.createMissile()).toBeInstanceOf(Missile);
+    });
+});
+
+describe('ManufacturerTESLA class', () => {
+    test('ManufacturerTESLA getManufacturerName is TESLA', () => {
+        expect(TESLAmanufact.getManufacturerName()).toBe("TESLA");
+    });
+    test('ManufacturerTESLA createSensor return is type of Sensor and props are accessibles', () => {
+        const sensor1 = TESLAmanufact.createSensor(3,sensorType.RADAR,40);
+        expect(sensor1).toBeInstanceOf(SensorDefaut);
+        expect(sensor1.sensorType).toBe(sensorType.RADAR);
+        expect(sensor1.sensorPower).toBe(40);
+    });
+    test('ManufacturerTESLA createShield return is type of Shield', () => {
+        expect(TESLAmanufact.createShield()).toBeInstanceOf(Shield);
+    });
+    test('ManufacturerTESLA createMissile return is type of Missile', () => {
+        expect(TESLAmanufact.createMissile()).toBeInstanceOf(Missile);
+    });
 });
--- src/index.ts
+++ src/index.ts
@@ -6,23 +6,23 @@ import { Spaceship } from "./Spaceship"
 import { Events } from "./Event"
 
 
-const eventManager = EventManager.getInstannce()
-const commander = new Commander()
+const EventManager_ = EventManager.getInstannce()
+const Commander_ = new Commander()
 
 console.log("\n---------START---------\n")
 
-const NASAmanufact: ISensorManufacturer = ManufacturerFactory.createManufacturer("NASA")
-const TESLAmanufact: ISensorManufacturer = ManufacturerFactory.createManufacturer("TESLA")
+const ManufacturerNASA: ISensorManufacturer = ManufacturerFactory.createManufacturer("NASA")
+const ManufacturerTESLA: ISensorManufacturer = ManufacturerFactory.createManufacturer("TESLA")
 
 console.log('')
 
-const SensorRadar = NASAmanufact.createSensor(1,sensorType.RADAR,80)
-const SensorHeat = TESLAmanufact.createSensor(2,sensorType.HEAT,40)
+const SensorRadar = ManufacturerNASA.createSensor(1,sensorType.RADAR,80)
+const SensorHeat = ManufacturerTESLA.createSensor(2,sensorType.HEAT,40)
 
 console.log('')
 
-const Shield = NASAmanufact.createShield()
-const Missile = TESLAmanufact.createMissile()
+const Shield = ManufacturerNASA.createShield()
+const Missile = ManufacturerTESLA.createMissile()
 
 //--------------------------------------------//
 
@@ -30,19 +30,19 @@ const SHIELD_ON = new ShieldOnCommand(Shield)
 const SHIELD_OFF = new ShieldOffCommand(Shield)
 const MISSILE_LAUNCH = new MissileLaunchCommand(Missile)
 
-commander.register('SHIELD_ON', SHIELD_ON)
-commander.register('SHIELD_OFF', SHIELD_OFF)
-commander.register('MISSILE_LAUNCH',MISSILE_LAUNCH)
+Commander_.register('SHIELD_ON', SHIELD_ON)
+Commander_.register('SHIELD_OFF', SHIELD_OFF)
+Commander_.register('MISSILE_LAUNCH',MISSILE_LAUNCH)
 
 console.log("\n------------------\n")
 
-const SpaceCadet = new Spaceship("SPACECADET",SensorRadar,SensorHeat,Shield,Missile,commander)
+const SpaceCadet = new Spaceship("SPACECADET",SensorRadar,SensorHeat,Shield,Missile,Commander_)
 
-eventManager.on(Events.EVT_ACTION, SpaceCadet.sensor1)
-eventManager.on(Events.EVT_ACTION, SpaceCadet.sensor2)
-eventManager.on(Events.EVT_ACTION, SpaceCadet)
-eventManager.on(Events.EVT_ORDER, SpaceCadet)
-eventManager.emit(Events.EVT_ACTION, {value : 80})
+EventManager_.on(Events.EVT_ACTION, SpaceCadet.sensor1)
+EventManager_.on(Events.EVT_ACTION, SpaceCadet.sensor2)
+EventManager_.on(Events.EVT_ACTION, SpaceCadet)
+EventManager_.on(Events.EVT_ORDER, SpaceCadet)
+EventManager_.emit(Events.EVT_ACTION, {value : 80})
 
 
 console.log("\n------------------\n")
